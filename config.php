00fde7a8a8fb12fef36798deab51d4a147a88676
zhaobinforward <312847654@qq.com>
2017-05-20 17:45:37 +0800

    base

---
+++ config.php
@@ -0,0 +1,73 @@
+<?php
+
+header('content-type:text/html;charset=utf-8');
+//通用常量
+define('R_ROOT', dirname(__FILE__));
+define('DATA_ROOT', R_ROOT . '/data');
+define('CACHE_ROOT', DATA_ROOT . '/cache');
+define('TPL_FILEEXT', 'html');//tpl suffix
+define('PRODUCT_MODEL', product_model()?1:0);//产品模式[1:产品模式,0:开发模式]
+define('TABLE_PREFIX', 'skindesignjxh2017_');//table prefix
+define('SITE_AUTHKEY', 'sogoushouji~!@');//站点加密串
+define('COOKIE_DOMAIN', '.shouji.sogou.com');
+define('TIMESTAMP', time());//入口时间戳
+define('CLOUD_URL', 'http://img.sogoucdn.com/app/a/100540022/');//云图url,一定要以/结尾
+define('PC_PIC','http://dl.pinyin.sogou.com/cache/skins/');//pc 皮肤图片cdn地址前缀
+define('PC_DL','http://download.pinyin.sogou.com/skins/download.php?skin_id=');//pc皮肤下载地址
+define('WAPDL_URL', 'http://img.shouji.sogou.com/wapdl/');//wapdl url,一定要以/结尾
+define('IN_APP', !0);//
+define('INTERFACE_KEY', 'UICHINA2016!@#');
+define('PHP7',substr(PHP_VERSION,0,1)==7 ? TRUE : FALSE );
+
+date_default_timezone_set('PRC');
+if(PHP_VERSION < '5.3.0'){
+	set_magic_quotes_runtime(0);//later 5.3.0, discard
+}
+
+if(PRODUCT_MODEL){
+	error_reporting(0);
+	ini_set('display_errors', 'Off');
+} else {
+	error_reporting(E_ERROR | E_WARNING | E_PARSE | E_CORE_ERROR);
+	ini_set('display_errors', 'On');
+}
+ob_start();
+@session_start();
+
+define('R_DATE_START', strtotime('2017-02-18 10:00:00'));//活动开始时间
+define('R_DATE_END', strtotime('2017-05-10 23:59:59'));//活动结束时间
+define('R_TODY_START', strtotime(date('Y-m-d')));//今日开始时间戳
+define('R_TODY_OFFSET', TIMESTAMP-R_TODY_START);//当前时间戳与今日开始时间戳的差量(即时间偏移量)
+
+//require R_ROOT.'/include/inc.constants.php';
+require R_ROOT.'/include/func.common.php';
+if(PHP7){
+	include R_ROOT.'/include/class.mysql7.php';
+}else{
+	include R_ROOT.'/include/class.mysql.php';
+
+}
+
+global $_G;
+$_G['uid'] = 0;
+$_G['nickname'] = '';
+$_G['user'] = array();
+$_G['ips'] = get_onlineip();
+$_G['onlineip'] =  empty($_G['ips']) || strpos($_G['ips'], ',') === false ? $_G['ips'] : substr($_G['ips'], 0, strpos($_G['ips'], ','));
+$_G['cookies'] = $_COOKIE;
+
+//DB
+$MDB = new nMysql('sogou_shoujiwap', array('charset'=>'utf8'));
+$SDB = &$MDB;
+
+/**	是否产品模式
+ *	@param void
+ *	@return Boolean
+ */
+function product_model() {
+	if($_SERVER['HTTP_HOST'] == '127.0.0.1' || $_SERVER['HTTP_HOST'] == 'localhost' || $_SERVER['HTTP_HOST'] == '192.168.1.100') {
+		return false;
+	}
+	return true;
+}
+?>
---
+++ include/functions.php
@@ -0,0 +1,41 @@
+﻿<?php
+
+
+/**	设置storage cookie
+ *	@param $info Array 数据
+ *	@return Boolean
+ */
+function set_storage($info) {
+	return ssetcookie('storage', @json_encode($info));
+}
+
+function create_guid($namespace = '') {
+	static $guid = '';
+	$uid = uniqid('', true);
+	$data = $namespace;
+	$data .= isset($_SERVER['REQUEST_TIME']) ? $_SERVER['REQUEST_TIME'] : '';
+	$data .= isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : '';
+	$data .= isset($_SERVER['LOCAL_ADDR']) ? $_SERVER['LOCAL_ADDR'] : '';
+	$data .= isset($_SERVER['LOCAL_PORT']) ? $_SERVER['LOCAL_PORT'] : '';
+	$data .= isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : '';
+	$data .= isset($_SERVER['REMOTE_PORT']) ? $_SERVER['REMOTE_PORT'] : '';
+	$hash = strtolower(hash('ripemd128', $uid . $guid . md5($data)));
+	$guid = substr($hash, 0, 8) . '-' . substr($hash, 8, 4) . '-' . substr($hash, 12, 4) . '-' . substr($hash, 16, 4) . '-' . substr($hash, 20, 12);
+	return $guid;
+}
+
+//获取或生成唯一uid
+function gen_uuid() {
+	if(isset($_COOKIE['uuid']) && strlen($_COOKIE['uuid']) > 0) {
+		$uuid = $_COOKIE['uuid'];
+	} else if(isset($_SESSION['uuid']) && strlen($_SESSION['uuid']) > 0) {
+		$uuid = $_SESSION['uuid'];
+	} else {
+		$uuid = create_guid();
+		$_SESSION['uuid'] = $uuid;
+		ssetcookie('uuid', $uuid);
+	}
+	return $uuid;
+}
+
+?>
