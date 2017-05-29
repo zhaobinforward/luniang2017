d5482746845b33650a86e3ce22e0afe4cb036b90
zhaobinforward <312847654@qq.com>
2017-05-29 22:57:45 +0800

    update

--- include/class.lottery.php
+++ include/class.lottery.php
@@ -42,6 +42,22 @@ interface LotteryIf
      * return array
      * */
     public function genRewardInfo($iOffset);
+
+
+    /*
+    * 根据uuid确认是否已中过奖
+    * @param $sUuid string 用户的唯一uuid
+    * return int
+    * */
+    public function isRewarded($sUuid);
+
+    /*
+    * 根据uuid查询中奖相应字段值
+    * @param $sUid string uid
+    * @param $sFeild string 字段名
+    * return mixed
+    * */
+    public function getPrizeInfoByUid($sUid, $sFeild);
 }
 
 class Lottery implements LotteryIf{
@@ -390,8 +406,10 @@ class Lottery implements LotteryIf{
             $sSql = "select status from luniang_2017_prize where uuid='".$sUuid."'";
             $oQuery = $this->_MDB->Query($sSql);
             $aRes = $this->_MDB->FetchArray($oQuery);
-            if(isset($aRes['status'])){
+            if($aRes){
                 return $aRes['status'];
+            }else{
+                return 0;
             }
         }else{
             return 0;
@@ -445,6 +463,24 @@ class Lottery implements LotteryIf{
         }
         return $aRes;
 
+    }
+
+
+    /*
+     * 根据uuid查询中奖相应字段值
+     * @param $sUid string uid
+     * @param $sFeild string 字段名
+     * return mixed
+     * */
+    public function getPrizeInfoByUid($sUid, $sFeild){
+        $sSql = "select ".$sFeild." from luniang_2017_prize where uuid='".$sUid."'";
+        $oQuery = $this->_MDB->Query($sSql);
+        $aRes = $this->_MDB->FetchArray($oQuery);
+        if($aRes[$sFeild]){
+            return $aRes[$sFeild];
+        }else{
+            return '';
+        }
     }
 
 }
